1ef886a206a4e791b6bd7857008f2634bae97f94
aerocyber <aerocyberdev@protonmail.com>
2022-05-25 12:32:24 +0530

    Add initial code for add.html

--- app/js/pagecontrol.js
+++ app/js/pagecontrol.js
@@ -23,3 +23,10 @@ function loadData() {
       '<div><div class="uk-card uk-card-primary uk-card-hover uk-card-body uk-light"><h3 class="uk-card-title">results[x].name</h3><p>URL: <a href="results[x].url">results[x].url</a></p><p>Categories: results[x].categories</p><p>Actions:</p></div></div>';
   }
 }
+
+function addItem() {
+  var name = document.getElementById("Name").value;
+  var url = document.getElementById("Url").value;
+  var cat = document.getElementById("Cat").value.split(",");
+  insertData((Name = name), (Url = url), (Categories = cat));
+}
